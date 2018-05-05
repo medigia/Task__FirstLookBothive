2a87045602b6ca93fc44d111603d6952a3a50649
Mike <mike_b_sorensen@live.dk>
2018-05-05 13:11:32 +0200

    Login template and js
    Login template and Javascript section for this is made ready for production

---
+++ template/login/login.view.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 05-05-2018
+ * Time: 13:00
+ */
+?>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <header>
+
+        </header>
+
+        <section id="login_area">
+            <div class=""
+        </section>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
