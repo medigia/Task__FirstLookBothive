7e1c9040cb8a7a6b9355cb42223407936bd73b91
Mike <mike_b_sorensen@live.dk>
2018-05-05 12:54:04 +0200

    API Core structure
    Api key functionality and endpoint routing.

--- core/core.api.php
+++ core/core.api.php
@@ -6,4 +6,28 @@
 */
 class API extends system {
 
+    protected $apikey = 'A3622LksldWW239AALKS';
+
+    function __construct() {
+        if($_GET['ep']) :
+            $this->findEndpoint($GET['ep']);
+        endif;
+    }
+
+    public function findEndpoint($endpoint) {
+        $this->currentEndpoint = $endpoint;
+        $this->$endpoint();
+    }
+
+    public function checkApiKey($apiKey) {
+        if($this->apikey == $apiKey) :
+            return true;
+        else :
+            return false;
+        endif;
+    }
+
+    function __destruct() {
+
+    }
 }
