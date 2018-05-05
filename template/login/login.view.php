c353a0446914684626757c8c1d38a4cb42422c3e
Mike <mike_b_sorensen@live.dk>
2018-05-05 13:47:14 +0200

    Update to Login tmpl and Styles.

--- template/login/login.view.php
+++ template/login/login.view.php
@@ -11,9 +11,10 @@
         <link rel="stylesheet" type="text/css" href="../../style/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="../../style/bootstrap-grid.css">
         <link rel="stylesheet" type="text/css" href="../../style/bootstrap-reboot.css">
+        <link rel="stylesheet" type="text/css" href="../../style/login.styles.css">
     </head>
 
-    <body>
+    <body id="login">
         <div class="container">
             <header id="header-outer">
 
@@ -21,21 +22,31 @@
 
             <section id="login_area">
                 <div class="row">
-                    <h2>Welcome to Bothive</h2>
+                    <div class="image-holder" id="login-logo">
+                        <img class="image-responsive image-bothive" src="../../images/BotHive.png" alt="BotHive Logo" />
+                    </div>
+                </div>
+                <div class="row">
+                    <h2>Velkommen Til Bothive</h2>
                     <p>Lorem feed ipsum halløj med dig</p>
+                </div>
+                <div class="row">
                     <hr>
+                </div>
+                <div class="row">
                     <div class="form-container">
-                        <div class="input-group">
-                            <label for="username">
-                                Brugernavn <input type="text" name="username" id="username">
-                            </label>
+                        <div class="form-group">
+                            <label for="firstname">Fornavn</label>
+                            <input type="text" class="form-control" id="firstname" aria-describedby="firstname_help" placeholder="Indsæt venligst dit fornavn">
                         </div>
-                        <div class="input-group">
-                            <label for="password">
-                                Kodeord <input type="password" name="password" id="password"
-                            </label>
+
+                        <div class="form-group">
+                            <label for="lastname">Efternavn</label>
+                            <input type="text" class="form-control" id="lastname" aria-describedby="lastname_help" placeholder="Indsæt venligst dit efternavn">
+                            <small id="lastname_help" class="form-text text-muted">Lad os vise dig en verden af teknologi der ikke er set magen til før.</small>
                         </div>
-                        <div class="input-group">
+                    </div>
+                    <div class="input-group">
                             <a href="#" name="submit-login" id="submit-login" class="btn btn-submit">Login</a>
                         </div>
                     </div>
